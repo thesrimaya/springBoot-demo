737df1901fa1e4d4c7e9a1d23741f8aed418f55f
srimaya <srimayaansumangouda@gmail.com>
2025-12-28 00:38:09 +0530

    aaaa

---
+++ src/main/java/com/example/demo/controller/BankController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/Axis")
+public class BankController {
+	@GetMapping("/welcome")
+	public String welcome() {
+	    return "welcome To Axis Bank";
+	}
+
+}
